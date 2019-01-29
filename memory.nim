c510bd2d106aa8363f4403c7b729455ddd1582b5
Tobias Frilling <tobias@frilling-online.de>
2019-01-29 14:10:15 +0100

    Small refactor of memory

--- memory.nim
+++ memory.nim
@@ -32,12 +32,12 @@ for line in lines("/proc/meminfo"):
     elif line.match(re"SwapFree:\s*(\d+)", matches):
         swap_free += parseInt(matches[0])
 
-var percentage: int
+var percentage: float
 if getEnv("BLOCK_INSTANCE") == "swap":
-    percentage = int(100*(1-swap_free/swap_total))
+    percentage = 1-swap_free/swap_total
 else:
-    percentage = int(100*(1-mem_free/mem_total))
+    percentage = 1-mem_free/mem_total
 
-echo percentage, "%" # full_text
-echo percentage, "%" # short_text
-echo GreenToRed(percentage/100) # color
+echo int(100*percentage), "%" # full_text
+echo int(100*percentage), "%" # short_text
+echo GreenToRed(percentage) # color
