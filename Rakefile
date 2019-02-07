d03b791f72805701878c2cfe7c395fe8392e2e4d
Tobias Frilling <tobias@frilling-online.de>
2019-02-07 18:00:53 +0100

    Add battery

--- Rakefile
+++ Rakefile
@@ -1,13 +1,13 @@
 require 'rake/clean'
 
-nim_files = Rake::FileList.new("*.nim")
+nim_files = Rake::FileList.new("memory.nim", "cpu_usage.nim", "battery.nim")
 
 task :default => :nim
 task :nim => nim_files.ext("")
 
 nim_files.each do |f|
     output = File.basename(f,".nim")
-    file output => f do
+    file output => [f, "colorscale.nim"] do
         sh "nim compile -d:release --opt:size #{f}"
         sh "strip -s #{output}"
     end
---
+++ battery.nim
@@ -0,0 +1,52 @@
+# Copyright 2019 Tobias Frilling
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import strutils, ospaths, math, strformat
+import colorscale
+
+proc readInt(f: string): int =
+    open(f).readLine().parseInt()
+
+let
+    path = "/sys/class/power_supply/" & getEnv("BLOCK_INSTANCE") & "/"
+    status = if open(path & "status").readLine() == "Charging": "C" else: "D"
+
+    capacity = readInt(path & "capacity")
+    current_now = readInt(path & "current_now")
+    charge_now = readInt(path & "charge_now")
+    charge_full = readInt(path & "charge_full")
+
+var remaining_time: float
+
+if status == "D":
+    remaining_time = charge_now/current_now
+else:
+    remaining_time = (charge_full - charge_now)/current_now
+
+let
+    remaining_hours = int(remaining_time)
+    remaining_minutes = int((remaining_time - floor(remaining_time)) * 60)
+    short_text = fmt"{capacity}% {status}"
+    full_text = short_text & fmt" {remaining_hours}:{remaining_minutes:02}"
+
+
+
+# Don't print remaining time if battery is full
+if remaining_time > 0:
+    echo full_text
+else:
+    echo short_text
+echo short_text
+if status == "D":
+    echo GreenToRed(1 - capacity/100)
